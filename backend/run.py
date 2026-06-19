b778995ce1551117addaed37b608af0c77137662
Fazri Gading <fazrigading@gmail.com>
2026-06-19 20:57:52 +0800

    migrate: completed phase 0

--- backend/run.py
+++ backend/run.py
@@ -1,6 +1,4 @@
-from app import create_app
-
-app = create_app()
+import uvicorn
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    uvicorn.run("app.main:app", host="0.0.0.0", port=5000, reload=True)
